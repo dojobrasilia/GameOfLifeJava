8925a42fd919a09c4241b5f748c3cb52aa186605
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-05-16 18:40:19 -0300

    Achamos que terminamos

--- src/Game.java
+++ src/Game.java
@@ -1,23 +1,39 @@
-import java.awt.Point;
-import java.util.HashSet;
-import java.util.Set;
 
 
 public class Game {
 
-	private Set<Point> alive = new HashSet<Point>();
+	private boolean[][] cells = new boolean[10][10];
 	
 	public void populate(int x, int y) {
-		alive.add(new Point(x,y));
+		cells[x][y]=true;
 	}
 
 	public void next() {
-		// TODO Auto-generated method stub
-		
+		boolean[][] nextGen = new boolean[10][10];
+		for(int x = 0; x < 10; x++)
+			for(int y = 0 ; y< 10; y++)
+				if (check(x,y)){
+					if (count(x,y) == 3 || count(x,y) == 2 ) nextGen[x][y] = true;
+					else nextGen[x][y] = false;
+				}else{
+					if (count(x,y) == 3) nextGen[x][y] = true;
+				}
+		cells=nextGen;
 	}
 
 	public boolean check(int x, int y) {
-		return alive.contains(new Point(x,y));
+		if (x < 0 || y < 0 || y > 9 || x > 9) return false;
+		return cells[x][y];
+	}
+
+	int count(int x, int y) {
+		int sum = 0;
+		for (int i = -1 ; i < 2; i++)
+			for (int j = -1 ; j < 2; j++)
+				if (!(i == 0 && j == 0) && check(x+j,y+i)) sum++;
+		
+		return sum;
 	}
 
 }
+
--- src/GameTest.java
+++ src/GameTest.java
@@ -20,12 +20,97 @@ public class GameTest {
 		assertTrue("(1,2) should be True",game.check(1,2));
 	}
 	
+	@Test public void aLonelyCellMustDie(){
+		Game game = new Game();
+		game.populate(0,0);
+		game.next();
+		assertFalse(game.check(0,0));
+	}
 	
+	@Test public void twoNeighboursCellsMustDie(){
+		Game game = new Game();
+		game.populate(1,2);
+		game.populate(1,3);
+		game.next();
+		assertFalse(game.check(1,2));
+		assertFalse(game.check(1,3));
+	}
 	
-//	@Test public void aLonelyCellMustDie(){
-//		Game game = new Game();
-//		game.populate(0,0);
-//		game.next();
-//		assertFalse(game.check(0,0));
-//	}
+	@Test public void threeCellsInARowMakesTheNeighourRiseFromTheDead(){
+		Game game = new Game();
+		game.populate(1,2);
+		game.populate(1,3);
+		game.populate(1,4);
+		game.next();
+		assertTrue(game.check(2,3));
+		assertTrue(game.check(0,3));
+	}
+	
+	@Test public void aCellWithTwoNeigboursLives(){
+		Game game = new Game();
+		game.populate(0,0);
+		game.populate(1,1);
+		game.populate(0,2);
+		game.next();
+		assertTrue(game.check(1,1));
+	}
+	
+	@Test public void aCellWithThreeNeigboursLives(){
+		Game game = new Game();
+		game.populate(0,0);
+		game.populate(0,1);
+		game.populate(1,1);
+		game.populate(0,2);
+		game.next();
+		assertTrue(game.check(1,1));
+	}
+	
+	@Test public void aCellWithFourNeigboursMustDie(){
+		Game game = new Game();
+		game.populate(0,0);
+		game.populate(1,0);
+		game.populate(2,0);
+		game.populate(0,1);
+		game.populate(1,1);
+		game.next();
+		assertFalse(game.check(1,0));
+	}
+	
+	@Test public void aDeadCellWithTwoNeigboursStaysDead(){
+		Game game = new Game();
+		game.populate(0,0);
+		game.populate(1,1);
+		game.next();
+		assertFalse(game.check(1,0));
+	}
+	
+	@Test public void countNeighboursForASingleLivingCell(){
+		Game game = new Game();
+		game.populate(2,2);
+		assertEquals(1,game.count(1,1));
+		assertEquals(1,game.count(2,1));
+		assertEquals(1,game.count(3,1));
+		assertEquals(1,game.count(1,2));
+		assertEquals(0,game.count(2,2));
+		assertEquals(1,game.count(3,2));
+		assertEquals(1,game.count(1,3));
+		assertEquals(1,game.count(2,3));
+		assertEquals(1,game.count(3,3));
+	}
+	
+	@Test public void countNeighboursForTwoLivingCell(){
+		Game game = new Game();
+		game.populate(2,2);
+		game.populate(2,3);
+		assertEquals(2,game.count(3,2));
+		assertEquals(1,game.count(3,1));
+	}
+	
+	@Test public void countBorders(){
+		Game game = new Game();
+		game.populate(0,1);
+		assertEquals(1,game.count(0,0));
+		assertEquals(0,game.count(9,9));
+	}
+	
 }
